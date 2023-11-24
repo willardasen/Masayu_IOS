319fdf3e7be7d3593c930b672f76ec5e28a641df
willardasen <willard.tio@gmail.com>
2023-11-24 02:21:36 -0800

    update

---
+++ Masayu/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  Masayu
+//
+//  Created by prk on 11/24/23.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
