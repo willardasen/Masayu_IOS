7f3eca368f0caca1352bfde212632dce4855fa12
willardasen <willard.tio@gmail.com>
2023-12-01 01:57:14 -0800

    validate email password database di login masi blm bs

--- Masayu/LoginViewController.swift
+++ Masayu/LoginViewController.swift
@@ -6,23 +6,77 @@
 //
 
 import UIKit
+import CoreData
+
+
+
+struct userData {
+    var email: String
+    var password: String
+}
 
 class LoginViewController: UIViewController {
-
     
-    @IBOutlet weak var loginTxt: UITextField!
+    //let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var arrUserData = [userData]()
+
+    var context: NSManagedObjectContext!
+    
+    @IBOutlet weak var emailTxt: UITextField!
     
     @IBOutlet weak var passwordTxt: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
-        // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        context = appDelegate.persistentContainer.viewContext
     }
     
 
     @IBAction func loginBtn(_ sender: Any) {
         
+//        fetchUserData()
+        
+        guard let email = emailTxt.text, !email.isEmpty else {
+            showAlert(title: "Email is empty", message: "Email must not be empty")
+            return
+        }
+        guard let password = passwordTxt.text, !password.isEmpty else {
+            showAlert(title: "Password is empty", message: "Password must not be empty")
+            return
+        }
+        
+        if !(email.hasSuffix(".com") && email.contains("@")){
+            showAlert(title: "Email is not valid", message: "Email must contain @ and ends with .com")
+        }
+
     }
+    
+//    func fetchUserData(){
+//        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+//        do{
+//            var results = try context.fetch(request) as! [NSManagedObject]
+//            for data in results{
+//                arrUserData.append(userData(email: data.value(forKey: "email") as! String, password: data.value(forKey: "password") as! String))
+//            }
+//            print("Fetching successful")
+//        }catch{
+//            print("Fetching failed")
+//        }
+//    }
+    
+    
+    func showAlert(title: String,message: String) {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    
+    
     
 }
--- Masayu/RegisterViewController.swift
+++ Masayu/RegisterViewController.swift
@@ -6,25 +6,101 @@
 //
 
 import UIKit
+import CoreData
 
 class RegisterViewController: UIViewController {
+    
+    var arrUser: [String] = []
+    var context: NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
 
-        // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        context = appDelegate.persistentContainer.viewContext
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBAction func registerBtn(_ sender: Any) {
+        fetchUserData()
+        
+        guard let username = usernameTxt.text, !username.isEmpty else {
+            showAlert(title: "Username is empty", message: "Username must not be empty")
+            return
+        }
+        
+        guard let email = emailTxt.text, !email.isEmpty else {
+            showAlert(title: "Email is empty", message: "Email must not be empty")
+            return
+        }
+        
+        guard let password = passwordTxt.text, !password.isEmpty else {
+            showAlert(title: "Password is empty", message: "Password must not be empty")
+            return
+        }
+        
+        
+        if username.count < 2{
+            showAlert(title: "Username is less than 2 letters", message: "Username must at least has 2 letters")
+        }
+        
+        if email.count < 5 {
+            showAlert(title: "Email is less than 5 letters", message: "Email must at least has 5 letters")
+        }
+        
+        if !(email.hasSuffix(".com") && email.contains("@")){
+            showAlert(title: "Email is not valid", message: "Email must contain @ and ends with .com")
+        }
+        
+        
+        
+        if(arrUser.contains(email)){
+            showAlert(title: "Email existed", message: "This email has already existed")
+        }
+        
+        let entityTarget = NSEntityDescription.entity(forEntityName: "User", in: context)
+        if(entityTarget != nil){
+            let newUser = NSManagedObject(entity: entityTarget!, insertInto: context)
+            newUser.setValue(username, forKey: "username")
+            newUser.setValue(email, forKey: "email")
+            newUser.setValue(password, forKey: "password")
+        }
+        
+        do{
+            try context.save()
+            print("Save successful")
+        }catch{
+            print("register error")
+        }
+        
     }
-    */
-
+    
+    func fetchUserData(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        do{
+            var results = try context.fetch(request) as! [NSManagedObject]
+            for data in results{
+                arrUser.append(data.value(forKey: "email") as! String)
+            }
+            print("Fetching successful")
+        }catch{
+            print("Fetching failed")
+        }
+    }
+    
+    
+    func showAlert(title: String,message: String) {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+    }
 }
